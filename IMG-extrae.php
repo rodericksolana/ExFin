578ec4719dee500c34b7d301a2d728ce10634fbe
Rodrigo Solana Chao <daw-1129839@ubiquitous.csf.itesm.mx>
2016-11-30 01:45:14 -0500

    Servidor

---
+++ IMG-extrae.php
@@ -0,0 +1,23 @@
+<?php
+// Creamos conexion
+//$conn = new mysqli("localhost", "rulo", "654321", "raulms");
+$conn = new mysqli("localhost", "1129839_user", "1129839", "daw_1129839");
+// Validamos la conexion
+if ($conn->connect_error) {
+    die("Conexion fallida: " . $conn->connect_error);
+}
+// hacemos el query
+$sql = "SELECT id, imagen FROM imagenes";
+$result = $conn->query($sql);
+while ($rows = $result->fetch_assoc()){
+echo $rows['id'];
+echo "<br><br>";
+echo "<img src=\"IMG-muestra-imagen.php?id=".$rows['id']."\">";
+echo "<br><br>";
+}
+// Cerramos la conexion a la base de datos
+$conn->close();
+
+
+?>
+
