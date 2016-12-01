a49ab54698f6f3fccbc3ace41c11e69dbdd8b124
rodericksolana <you@example.com>
2016-11-30 20:45:10 -0600

    Avanzando

--- submit.php
+++ submit.php
@@ -16,11 +16,11 @@ if(isset($_POST["login"])){
     $eMail=$_POST["eMail"];
     if(!empty($eMail) && !empty($Pwd)){
         $sql = "select idUsuario from exf_Usuarios where eMail='$eMail' and Pwd= '$Pwd'";
-        echo $sql;
+        //echo $sql;
         $num = mysqli_query($con, $sql) or die ('Query incorrecto: ' . $sql);
         //echo "Terminé el query";
         //echo $num;
-        if($num == 1) {
+        if($num != empty) {
             echo "success";
 
            // header("Location:cliente.html");
