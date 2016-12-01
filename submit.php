2d2d9b20a9be6da804aac1d106761231d16de393
rodericksolana <you@example.com>
2016-11-30 20:47:25 -0600

    Avanzando

--- submit.php
+++ submit.php
@@ -20,7 +20,7 @@ if(isset($_POST["login"])){
         $num = mysqli_query($con, $sql) or die ('Query incorrecto: ' . $sql);
         //echo "Terminé el query";
         //echo $num;
-        if($num != empty()) {
+        if(!empty($num)) {
             echo "success";
 
            // header("Location:cliente.html");
