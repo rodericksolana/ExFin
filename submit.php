e67a27b0a5020068aa8aa1116c9152eff43a6fd5
rodericksolana <you@example.com>
2016-11-30 20:42:42 -0600

    Avanzando

--- submit.php
+++ submit.php
@@ -19,7 +19,7 @@ if(isset($_POST["login"])){
         echo $sql;
         $num = mysqli_query($con, $sql) or die ('Query incorrecto: ' . $sql);
         //echo "Terminé el query";
-        echo $num;
+        //echo $num;
         if($num == 1) {
             echo "success";
 
