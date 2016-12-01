b1b9432e3c470e54a33d268f917e1fa968041c00
rodericksolana <you@example.com>
2016-12-01 02:22:02 -0600

    Avanzando

---
+++ backEnd/IMG-extrae.php
@@ -0,0 +1,40 @@
+<?php
+// Creamos conexion
+//$conn = new mysqli("localhost", "rulo", "654321", "raulms");
+$conn = new mysqli("localhost", "1129839_user", "1129839", "daw_1129839");
+// Validamos la conexion
+if ($conn->connect_error) {
+    die("Conexion fallida: " . $conn->connect_error);
+}
+// hacemos el query
+$sql = "SELECT idViajes, NombreDestino, Fotografias, Descripcion FROM exf_Viajes order by idViajes desc limit 6";
+$result = $conn->query($sql);
+while ($rows = $result->fetch_assoc()){
+//echo $rows['id'];
+    echo "<div class=\"col-md-4 col-sm-6 portfolio-item\">
+            <a href=\"#portfolioModal6\" class=\"portfolio-link\" data-toggle=\"modal\">
+                <div class=\"portfolio-hover\">
+                            <div class=\"portfolio-hover-content\">
+                                <i class=\"fa fa-plus fa-3x\"></i>
+                            </div>
+                        </div>";
+//echo "<br><br>";
+echo "<img class=\"img-responsive\" src=\"".$rows['Fotografias']."\">";
+  //  echo "<img src =\"http://mediawiki.talentum.natware.mx/images/thumb/c/c6/Quechulachiapas.2.jpg/400px-Quechulachiapas.2\">";
+//echo "<br><br>";
+// var_dump ($rows);
+// var_dump ($rows['Fotografias']);
+
+    echo "</a>
+                    <div class=\"portfolio-caption\">
+                        <h4> \"".$rows['NombreDestino']."\" </h4>
+                        <p class=\"text-muted\">\"".$rows['Descripcion']."\"</p>
+                    </div>
+                </div>";
+}
+// Cerramos la conexion a la base de datos
+$conn->close();
+
+
+?>
+
---
+++ backEnd/login.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: rodericksolana
+ * Date: 30/11/16
+ * Time: 04:41 PM
+ */
+
+//echo "Entre a login.php";
+require_once("config.php");
+
+class Database{
+
+    public function closeDataBase(){
+        $con=null;
+
+    }
+    /* this checks if the user exists
+    @param email is users email
+    @param $conn is the connection object
+    */
+    public function checkUserExist($eMail,$con)
+    {
+
+        $sql = "select count(*) from exf_Usuarios where eMail='$eMail'";
+        echo $sql;
+        $num = mysqli_query($con, $sql) or die ('Query incorrecto: ' . $sql);
+
+        echo "Esto vale num";
+        echo $num;
+
+
+        if($num>0)
+            return true;
+        else
+            return false;
+    }
+    /*
+    This method authenticates the user
+    @param email is users email
+    @param password is the submitted password
+    @param $conn is the connection object
+    */
+    public function authen($eMail,$Pwd,$con){
+        $sql="select Pwd from exf_Usuarios where eMail='$eMail'";
+        $st=$con->prepare($sql);
+        $st->bindValue(":eMail",$eMail);
+        $res=$st->execute();
+        $row=$st->fetchAll();
+        if(password_verify($Pwd,$row[0]['Pwd']))
+            return true;
+        else
+            return false;
+    }
+
+    public function register($eMail,$Pwd,$con){
+        $hash=password_hash($Pwd,PASSWORD_DEFAULT);//we hash  our password
+        $sql="INSERT INTO exf_Usuarios( eMail, Pwd, TipoUsuario ) VALUES ( :eMail, :Pwd ,0)";
+        $st=$con->prepare($sql);
+        $st->bindValue(":eMail",$eMail);
+        $st->bindValue(":Pwd",$hash);
+        $st->execute();
+        $st=null;
+    }
+}
+?>
