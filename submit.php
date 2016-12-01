aebe00f6b0f3a3e30bb5bbaeb0c224fbcd19c4c9
rodericksolana <you@example.com>
2016-11-30 21:05:38 -0600

    Avanzando

--- submit.php
+++ submit.php
@@ -25,7 +25,7 @@ if(isset($_POST["login"])){
         }
             else {
 
-                echo '<script language="javascript">alert("Datos Incorrectos");  header("Location:login.html");</script>';
+                echo '<script language="javascript">alert("Datos Incorrectos"); window.location = login.html;</script>';
 
             }
 
