254e6eeb14d1bc4caea0699f5a960aab255dc076
rodericksolana <you@example.com>
2016-12-03 13:30:56 -0600

    Avanzando

--- sessionID.php
+++ sessionID.php
@@ -8,6 +8,14 @@
 
 session_start();
 
+
+include_once("config.php");
+
+
+$sql = "select * from exf_Usuarios where idUsuario= $_SESSION[\"idSession\"]";
+
+        $nume = mysqli_query($con, $sql) or die ('Query incorrecto: ' . $sql);
+
 echo "Esto es lo que regreso:";
 
 echo "<div class=\"intro-lead-in\"> HOLA  " .  $_SESSION["idSession"] . "</div>";
